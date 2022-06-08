6ded4ffe041e8144ed76abfb882bcde4314da04a
Alumno <Alumno@E133L30102.esucomex.acad>
2022-06-08 19:32:01 -0400

    cambio de nombre Nilcer Guevara

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        qa Nilcer
+        main Nilcer Guevara   
         </p>
         <a
           className="App-link"
