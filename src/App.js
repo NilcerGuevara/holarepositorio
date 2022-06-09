1fdc79d83d6ab0ea270702bff7a969eb9c1c6ab8
Alumno <Alumno@E133L30102.esucomex.acad>
2022-06-08 20:13:37 -0400

    cambio de rama a master con Nilcer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         hola mundo de internet
+         saludos de parte de parte de un compañero
         </p>
         <a
           className="App-link"
